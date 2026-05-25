c6f7a5c81e053a7968661973ec4f29c672a9ad72
lil-kreed1 <mariavernikovskaia@icloud.com>
2026-05-25 17:17:28 +0300

    new

--- main_project/Student.h
+++ main_project/Student.h
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
 class Student {
@@ -13,75 +12,74 @@ public:
 	char class_letter;
 	bool alive;
 
+	//default constructor / constructor without arguments
 	Student() {
-		//cout << "student default constructor..." << endl;
+		cout << "Default costructor" << endl;
 		id = 0;
 		name = "";
 		age = 0;
-		marks = new double[3] {1, 2, 3};
 		class_number = 0;
 		class_letter = '\0';
+		marks = new double [3] {10, 9, 7};
 		alive = false;
 	}
 
+	//canonical constructor / constructor with arguments
 	Student(int i, string nm, int ag, int number, char letter, bool a) {
-		cout << "student canonical constructor..." << endl;
+		cout << "Student canonical constructor" << endl;
 		id = i;
 		name = nm;
 		age = ag;
 		class_number = number;
 		class_letter = letter;
+		marks = new double [3] {10, 9, 7};
 		alive = a;
 	}
 
-	Student(string nm, int ag) {
-		id = 0;
-		name = nm;
-		age = ag;
-		class_number = 0;
-		class_letter = '\0';
-		marks = 0;
-		alive = true;
-	}
-
+	//constructor with arguments
 	Student(int ag, string nm) {
+		cout << "Student constructor with arguments" << endl;
 		id = 0;
 		name = nm;
 		age = ag;
 		class_number = 0;
 		class_letter = '\0';
-		marks = 0;
+		marks = new double [3] {10, 9, 7};
 		alive = true;
 	}
 
+	//copy-constructor / constructor with arguments
 	Student(const Student& student) {
-		cout << "student copy-constructor..." << endl;
-		/*id = student.id;
+		cout << "Student copy-constructor" << endl;
+		id = student.id;
 		name = student.name;
 		age = student.age;
 		class_number = student.class_number;
 		class_letter = student.class_letter;
-		mark = student.mark;
-		alive = student.alive*/;
+		marks = new double[3] {student.marks[0],
+			student.marks[1],
+			student.marks[2]};
+		alive = student.alive;
 	}
 
 	~Student() {
-		cout << "student destructor..." << endl;
+		cout << "Student destructor" << endl;
 		if (marks != NULL) {
 			delete[] marks;
 		}
 	}
 
 	string toString() {
-		string s = "student: ";
-		s += name;
-		s += "[#" + to_string(id) + "]";
+		string s = "Student: ";
+		s += "id = " + to_string(id);
+		s += ", name = " + name;
 		s += ", age = " + to_string(age);
-		s += ", class  = " + to_string(class_number) + to_string(class_letter);
-		s += ", marks = { " + to_string(marks[0]) + ", " + to_string(marks[1]) 
-			+ ", " + to_string(marks[2]) + " }";
+		s += ", class = " + to_string(class_number) + to_string(class_letter);
+		s += ", marks = {" + to_string(marks[0]) + ", " + to_string(marks[1])
+			+ ", " + to_string(marks[2]) + "}";
 		s += ", alive = ";
 		s += (alive ? "yes" : "no");
+
 		return s;
 	}
 };
--- main_project/main.cpp
+++ main_project/main.cpp
@@ -1,32 +1,33 @@
-#include "student.h"
-
-//void print(Student student) {
-//	cout << "in print function:\n";
-//	cout << student.toString() << endl;
+//#include "Student.h"
+//
+////void print(Student student) {
+////	cout << "In print function:\n";
+////	cout << student.toString() << endl;
+////}
+//
+////Student getBestStudent() {
+////	Student alex(123, "Alex", 15, 9, 'A', 10, true);
+////	return alex;
+////}
+//
+//int main() {
+//	Student alex(123, "Alex", 15, 9, 'A', true);
+//	Student vlad = alex;
+//
+//	cout << "Before: ";
+//	cout << alex.toString() << endl;
+//	cout << vlad.toString() << endl;
+//
+//	alex.id = 1000;
+//	alex.marks[0] = 5;
+//	alex.marks[1] = 5;
+//	alex.marks[2] = 5;
+//
+//	cout << "After: ";
+//	cout << alex.toString() << endl;
+//	cout << vlad.toString() << endl;
+//
+//	
+//
+//	return 0;
 //}
-
-//Student get_best_student() {
-//	Student alex(123, "alex", 14, 9, 'a', true);
-//	return alex;
-//}
-
-int main() {
-	Student alex(123, "alex", 14, 9, 'a', true);
-
-	Student vlad = alex;
-
-	cout << "before: ";
-	cout << alex.toString() << endl;
-	cout << vlad.toString() << endl;
-
-	alex.id = 1000;
-	alex.marks[0] = 5;
-	alex.marks[1] = 5;
-	alex.marks[2] = 5;
-
-	cout << "after: ";
-	cout << alex.toString() << endl;
-	cout << vlad.toString() << endl;
-
-	return 0;
-}
