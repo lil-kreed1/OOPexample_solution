7b402af9bc72ca66cf2f739cc5c8256fb4a6db8d
lil-kreed1 <mariavernikovskaia@icloud.com>
2026-05-25 19:06:59 +0300

    new

--- main_project/Student.h
+++ main_project/Student.h
@@ -3,83 +3,39 @@
 using namespace std;
 
 class Student {
-public:
+private:
 	int id;
 	string name;
 	int age;
-	double* marks;
+	int* marks;
+	int count;
 	int class_number;
 	char class_letter;
 	bool alive;
 
-	//default constructor / constructor without arguments
-	Student() {
-		cout << "Default costructor" << endl;
-		id = 0;
-		name = "";
-		age = 0;
-		class_number = 0;
-		class_letter = '\0';
-		marks = new double [3] {10, 9, 7};
-		alive = false;
-	}
+public:
 
-	//canonical constructor / constructor with arguments
-	Student(int i, string nm, int ag, int number, char letter, bool a) {
-		cout << "Student canonical constructor" << endl;
-		id = i;
-		name = nm;
-		age = ag;
-		class_number = number;
-		class_letter = letter;
-		marks = new double [3] {10, 9, 7};
-		alive = a;
-	}
+	Student();
+	Student(int i, string nm, int ag, int number, char letter, bool a);
+	Student(int ag, string nm);
+	Student(const Student& student);
+	~Student();
 
-	//constructor with arguments
-	Student(int ag, string nm) {
-		cout << "Student constructor with arguments" << endl;
-		id = 0;
-		name = nm;
-		age = ag;
-		class_number = 0;
-		class_letter = '\0';
-		marks = new double [3] {10, 9, 7};
-		alive = true;
-	}
+	int get_id();
+	void set_id(int i);
+	string get_name();
+	void set_name(string nm);
+	int get_age();
+	void set_age(int a);
+	int get_class_number();
+	void set_class_number(int num);
+	char get_class_letter();
+	void set_class_letter(char letter);
+	bool is_alive();
+	void set_alive(bool al);
+	int* get_marks();
+	void set_marks(int* ms, int c);
+	int get_count();
 
-	//copy-constructor / constructor with arguments
-	Student(const Student& student) {
-		cout << "Student copy-constructor" << endl;
-		id = student.id;
-		name = student.name;
-		age = student.age;
-		class_number = student.class_number;
-		class_letter = student.class_letter;
-		marks = new double[3] {student.marks[0],
-			student.marks[1],
-			student.marks[2]};
-		alive = student.alive;
-	}
-
-	~Student() {
-		cout << "Student destructor" << endl;
-		if (marks != NULL) {
-			delete[] marks;
-		}
-	}
-
-	string toString() {
-		string s = "Student: ";
-		s += "id = " + to_string(id);
-		s += ", name = " + name;
-		s += ", age = " + to_string(age);
-		s += ", class = " + to_string(class_number) + to_string(class_letter);
-		s += ", marks = {" + to_string(marks[0]) + ", " + to_string(marks[1])
-			+ ", " + to_string(marks[2]) + "}";
-		s += ", alive = ";
-		s += (alive ? "yes" : "no");
-
-		return s;
-	}
+	string toString();
 };
